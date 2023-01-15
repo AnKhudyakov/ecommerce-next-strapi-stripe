8a260bedf0768f27df359b81703cc6f575504655
AnKhudyakov <andrew_job@list.ru>
2023-01-16 00:58:33 +0400

    work productCard path

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -13,35 +13,33 @@ import ProductCard from "../../src/components/ProductCard";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../../lib/theme";
-import { useRouter } from "next/router";
 
 const Id = ({ item, items, error, description }) => {
-  console.log("item", item);
+  //console.log("item", item);
   //console.log("description", description);
   const { user, loading } = useFetchUser();
-  //const { query } = useRouter();
-  //if (error) {
-  //     return (
-  //       <Provider store={store}>
-  //         <ThemeProvider theme={theme}>
-  //           <Layout>
-  //             <p>{error}</p>
-  //           </Layout>
-  //         </ThemeProvider>
-  //       </Provider>
-  //     );
-  //   } else {
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Layout user={user}>
-          {/* <ProductCard item={item} items={items} description={description} /> */}
-        </Layout>
-      </ThemeProvider>
-    </Provider>
-  );
+  if (error) {
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <p>{error}</p>
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    );
+  } else {
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Layout user={user}>
+            <ProductCard item={item} items={items} description={description} />
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    );
+  }
 };
-//};
 
 export async function getStaticPaths() {
   const products = await fetcher(
