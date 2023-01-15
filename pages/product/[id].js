c482fbf18cb742c744001e0e702c88af65cfdb45
AnKhudyakov <andrew_job@list.ru>
2023-01-16 00:53:07 +0400

    getstaticpath

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -15,62 +15,73 @@ import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../../lib/theme";
 import { useRouter } from "next/router";
 
-const Id = ({ id,item, items, error, description }) => {
-  console.log("ID", id);
+const Id = ({ item, items, error, description }) => {
+  console.log("item", item);
   //console.log("description", description);
   const { user, loading } = useFetchUser();
   //const { query } = useRouter();
   //if (error) {
-//     return (
-//       <Provider store={store}>
-//         <ThemeProvider theme={theme}>
-//           <Layout>
-//             <p>{error}</p>
-//           </Layout>
-//         </ThemeProvider>
-//       </Provider>
-//     );
-//   } else {
-    return (
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Layout user={user}>
-            {/* <ProductCard item={item} items={items} description={description} /> */}
-          </Layout>
-        </ThemeProvider>
-      </Provider>
-    );
-  }
+  //     return (
+  //       <Provider store={store}>
+  //         <ThemeProvider theme={theme}>
+  //           <Layout>
+  //             <p>{error}</p>
+  //           </Layout>
+  //         </ThemeProvider>
+  //       </Provider>
+  //     );
+  //   } else {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Layout user={user}>
+          {/* <ProductCard item={item} items={items} description={description} /> */}
+        </Layout>
+      </ThemeProvider>
+    </Provider>
+  );
+};
 //};
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  const products = await fetcher(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}products`
+  );
+  console.log(products.data);
+  const paths = products.data.map((product) => ({
+    params: { id: `${product.id}` },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
   const { id } = params;
-//   const ProductResponse = await fetcher(
-//     `${process.env.NEXT_PUBLIC_STRAPI_URL}products/${id}?populate=%2A`
-//   );
-//   const ProductsResponse = await fetcher(
-//     `${process.env.NEXT_PUBLIC_STRAPI_URL}products?populate=%2A`
-//   );
-//   console.log("ProductsResponse", ProductsResponse);
-//   if (ProductResponse.data) {
-//     const description = await markdownToHtml(
-//       ProductResponse.data.attributes.description
-//     );
+  const ProductResponse = await fetcher(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}products/${id}?populate=%2A`
+  );
+  const ProductsResponse = await fetcher(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}products?populate=%2A`
+  );
+  console.log("ProductsResponse", ProductsResponse);
+  if (ProductResponse.data) {
+    const description = await markdownToHtml(
+      ProductResponse.data.attributes.description
+    );
     return {
       props: {
-        id
-        //item: ProductResponse.data,
-       // items: ProductsResponse,
-       // description,
+        item: ProductResponse.data,
+        items: ProductsResponse,
+        description,
       },
     };
- // } else {
- //   return {
- //     props: {
- //       error: ProductResponse.error.message,
- //     },
-//    };
-//  }
+  } else {
+    return {
+      props: {
+        error: ProductResponse.error.message,
+      },
+    };
+  }
 }
 
 export default Id;
